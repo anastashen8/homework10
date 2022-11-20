53b850aa02bd57601840a3477b3652cb94dc2439
Anastasiya <anastashen8@gmail.com>
2022-11-20 22:01:18 +0300

    add Task58

---
+++ Task58/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет 
+// находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+Console.Write("Введите количество строк 1 массива: ");
+int rowsA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов 1 массива: ");
+int columnsA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество строк 2 массива: ");
+int rowsB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов 2 массива: ");
+int columnsB = Convert.ToInt32(Console.ReadLine());
+if (columnsA != rowsB)
+{
+  Console.WriteLine("Такие матрицы умножать нельзя!");
+  return;
+}
+
+int[,] Fill2DArrayA()
+{
+    int[,] array = new int[rowsA, columnsA];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+        }
+    }
+    return array;
+}
+int[,] Fill2DArrayB()
+{
+    int[,] array = new int[rowsB, columnsB];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+        }
+    }
+    return array;
+}
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] GetMultiplicationMatrix(int[,] arrayA, int[,] arrayB)
+{
+    int[,] arrayC = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+    for (int i = 0; i < arrayA.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayB.GetLength(1); j++)
+        {
+            for (int k = 0; k < arrayA.GetLength(1); k++)
+            {
+                arrayC[i, j] += arrayA[i, k] * arrayB[k, j];
+            }
+        }
+    }
+    return arrayC;
+}
+int[,] A = Fill2DArrayA();
+int[,] B = Fill2DArrayB();
+Print2DArray(A);
+Console.WriteLine();
+Print2DArray(B);
+Console.WriteLine();
+Console.WriteLine($"\nПроизведение двух матриц: ");
+Print2DArray(GetMultiplicationMatrix(A,B));
